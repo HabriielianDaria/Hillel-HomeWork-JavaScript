e697f9a1510a3c6dcc1bc3a7efb258db56088389
HabriielianDaria <47780657+HabriielianDaria@users.noreply.github.com>
2024-12-09 19:57:03 +0200

    Update js_hw7.2.js

--- js_hw7.2.js
+++ js_hw7.2.js
@@ -1,22 +1,3 @@
-/*function isValidString(str) {
-    const regex = /\b[^aA\s,]{6,}\b/g;
-    return regex.test(str);
-}
-
-var str = "Wonderful, Joyful, Happiness, Time, Task, Apple";
-
-console.log(isValidString(str));  
-console.log(isValidString("Wonderful")); 
-console.log(isValidString("Oxford"));    
-console.log(isValidString("H@ppiness")); 
-console.log(isValidString("Time"));      
-console.log(isValidString("Task"));      
-console.log(isValidString("Apple"));    
-console.log(isValidString("Daria4")); 
-console.log(isValidString("Dorio")); 
-console.log(isValidString("Dorio5")); */
-
-
 function isValidString(str) {
     const words = str.split(/\s*,\s*/); 
     const regex = /^[^aA\s,]{6,}$/;  
