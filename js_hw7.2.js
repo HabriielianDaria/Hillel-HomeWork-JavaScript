9af597c1796b3aae41b7266c105d8387534eb162
HabriielianDaria <47780657+HabriielianDaria@users.noreply.github.com>
2024-12-09 19:49:53 +0200

    Update js_hw7.2.js

--- js_hw7.2.js
+++ js_hw7.2.js
@@ -1,8 +1,11 @@
-function isValidString(str) {
-    const regex = /^[^aA]{6,}$/;
+/*function isValidString(str) {
+    const regex = /\b[^aA\s,]{6,}\b/g;
     return regex.test(str);
 }
 
+var str = "Wonderful, Joyful, Happiness, Time, Task, Apple";
+
+console.log(isValidString(str));  
 console.log(isValidString("Wonderful")); 
 console.log(isValidString("Oxford"));    
 console.log(isValidString("H@ppiness")); 
@@ -10,7 +13,24 @@ console.log(isValidString("Time"));
 console.log(isValidString("Task"));      
 console.log(isValidString("Apple"));    
 console.log(isValidString("Daria4")); 
-console.log(isValidString("Dorio")); //false бо 5 символів
-console.log(isValidString("Dorio5")); //true
+console.log(isValidString("Dorio")); 
+console.log(isValidString("Dorio5")); */
+
+
+function isValidString(str) {
+    const words = str.split(/\s*,\s*/); 
+    const regex = /^[^aA\s,]{6,}$/;  
+    words.forEach(word => {
+        console.log(`${word}: ${regex.test(word)}`);
+    });
+    return true; 
+}
+
+var str = "Wonderful, Joyful, Happiness, Time, Task, Apple";
+isValidString(str);
+
+console.log(isValidString("Daria4")); 
+console.log(isValidString("Dorio")); 
+console.log(isValidString("Dorio5"));
 
 
